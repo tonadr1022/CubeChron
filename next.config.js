7e5d2a7fb9dbceed0d55d5662a8d10c296aa2429
Tony Adriansen <tony.adriansen@gmail.com>
2023-07-30 11:21:09 -0500

    --no-edit

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   env: {
     GRAPHQL_URI: process.env.GRAPHQL_URI,
     DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
 };
 const withPWA = require("next-pwa")({
