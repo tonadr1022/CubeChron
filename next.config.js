baf4202b2f4313016329acd92b6408f3e300fbeb
Tony Adriansen <tony.adriansen@gmail.com>
2023-08-01 08:15:51 -0500

    --no-edit

--- next.config.js
+++ next.config.js
@@ -11,7 +11,8 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "*",
+        // source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
@@ -22,7 +23,7 @@ const nextConfig = {
           {
             key: "Access-Control-Allow-Headers",
             value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, X-File-Name, X-File-Size, ",
           },
         ],
       },
